2c4b38d3ec7826288f553d289c2d43e72ebfb646
Riccardo Fracescato <riccardo.francescato@gmail.com>
2017-03-21 16:21:16 +0100

    Added license

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,4 +1,29 @@
-<?php // no direct access
+<?php
+/**
+*    Joomla Event Countdown Module
+*    Copyright (C) 2017 Riccardo Francescato. All rights reserved.
+*    riccardo.francescato[at]gmail.com
+*
+*   This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @package  Event Countdown
+* @version   $Id: helper.php - March 2017 
+* @author    Riccardo Francescato (riccardo.francescato@gmail.com)
+* @copyright   Copyright (C) 2017 Riccardo Francescato. All rights reserved.
+* @license   https://github.com/RiccardoFrancescato/JoomlaCuntdown/LICENSE
+**/
+// No direct access
 defined ('_JEXEC') or die('Restricted access');
 $doc->addStyleSheet(JURI::root(true).'/modules/mod_countdown/css/style.css');
 $event_date2=DateTime::createFromFormat('m/d/Y', $event_date);
